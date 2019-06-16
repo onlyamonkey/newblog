fa5f3eb5ebc4397073b2cbcb0bcfdb2e36e42507
e <412763575@qq.com>
2019-06-16 22:17:38 +0800

    1

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/onlyamonkey/min_photos/master/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/onlyamonkey/photos/master/' + data.link[i];
+          var minSrc = 'https://github.com/onlyamonkey/newblog/blob/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/onlyamonkey/newblog/blob/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
